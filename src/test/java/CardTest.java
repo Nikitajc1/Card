d045fdade0ac53350a9b76eebaf9002320849f85
Nikita U <nikitajc@mail.ru>
2023-01-13 15:13:54 +0300

    Fixed

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -9,48 +9,86 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import javax.swing.*;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardTest {
 
+    @BeforeEach
+    void openChrome() {
+        open("http://localhost:9999/");
+    }
+
     @Test
     void testCard() {
-        open("http://localhost:9999/");
         $("[data-test-id=name] input").setValue("Иван");
         $("[data-test-id=phone] input").setValue("+79998887766");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void testNameFirst() {
+        $("[data-test-id=name] input").setValue("Иван Петров-Иванов");
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void invalidValueName() {
-        open("http://localhost:9999/");
         $("[data-test-id=name] input").setValue("123");
         $("[data-test-id=phone] input").setValue("+79998887766");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("span.input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void invalidValuePhone() {
-        open("http://localhost:9999/");
-        $("[data-test-id=name] input").setValue("Иван");
+        $("[data-test-id=name] input").setValue("Иван Иванов-Петров");
         $("[data-test-id=phone] input").setValue("+7999888776644");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=phone] span.input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void invalidValuePhoneAndNameShouldPointAtName() {
-        open("http://localhost:9999/");
         $("[data-test-id=name] input").setValue("Ivan");
         $("[data-test-id=phone] input").setValue("+7999888776644");
         $("[data-test-id=agreement]").click();
         $("button").click();
-        $("[data-test-id=name] span.input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void checkboxTest() {
+        $("[data-test-id=name] input").setValue("Иван");
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("button").click();
+        $("[data-test-id=agreement].input_invalid").should(Condition.exist);
+    }
+
+    @Test
+    void emptyName() {
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void emptyPhone() {
+        $("[data-test-id=name] input").setValue("Иван Петров-Иванов");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 }
